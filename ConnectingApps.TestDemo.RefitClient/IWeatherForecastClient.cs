5940771b1b8efa5451dea23567c1ba0e476c42c3
Daan Acohen <daan.acohen@connectingapps.net>
2020-05-05 12:11:37 +0200

    Added base class for testing with a refit client

---
+++ ConnectingApps.TestDemo.RefitClient/IWeatherForecastClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Refit;
+
+namespace ConnectingApps.TestDemo.RefitClient
+{
+    public interface IWeatherForecastClient
+    {
+        [Get("/weatherforecast")]
+        Task<ApiResponse<IEnumerable<WeatherForecast>>> GetForecast();
+    }
+}
---
+++ ConnectingApps.TestEnablers/RefitClientTestBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace ConnectingApps.TestEnablers
+{
+    public abstract class RefitClientTestBase<TStartup,TRefitClient> : IClassFixture<CustomWebApplicationFactory<TStartup>> where TStartup : class where TRefitClient : class
+    {
+        private HttpClient _httpClient;
+        protected readonly TRefitClient RefitClient;
+
+        protected RefitClientTestBase(CustomWebApplicationFactory<TStartup> factory, int portNumber, bool useHttps)
+        {
+            var extraConfiguration = GetConfiguration();
+            string afterHttp = useHttps ? "s" : "";
+            _httpClient = factory.WithWebHostBuilder(whb =>
+            {
+                whb.ConfigureAppConfiguration((context, configbuilder) =>
+                {
+                    configbuilder.AddInMemoryCollection(extraConfiguration);
+                });
+            }).CreateClient(new WebApplicationFactoryClientOptions
+            {
+                BaseAddress = new Uri($"http{afterHttp}://localhost:{portNumber}")
+            });
+            RefitClient = CreateRefitClient(_httpClient);
+        }
+
+        protected abstract TRefitClient CreateRefitClient(HttpClient httpClient);
+
+        protected virtual Dictionary<string, string> GetConfiguration()
+        {
+            return new Dictionary<string, string>();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _httpClient.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
